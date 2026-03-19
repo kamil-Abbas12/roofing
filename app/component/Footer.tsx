2af04ac741db773ba8cfcc7a69e576fe6bbf31cd
Kamil Abbas <kamilabbas929@gmail.com>
2026-03-19 22:43:40 +0500

    fix  s

--- app/component/Footer.tsx
+++ app/component/Footer.tsx
@@ -22,7 +22,7 @@ export default function Footer() {
           {/* LEFT SIDE */}
           <div className="max-w-md">
             <Image
-              src="/logo2.png"
+              src="/logo1.png"
               width={70}
               height={60}
               alt="Top Dog Roofing"
